d6a915c882319c965a40f8437dfd0fad6fd70927
Leonov-nick <leonov-nic@yandex.ru>
2021-02-11 23:31:06 +0300

    11 февраля футер-навигация

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,34 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+(function () {
+  var pageFooter = document.querySelector('.page-footer');
+  var footerToggles = document.querySelectorAll('.page-footer__toggles');
+  var lists = document.querySelectorAll('.page-footer__js-menu');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+  console.log(footerToggles);
+  console.log(lists);
+
+  pageFooter.classList.remove('page-footer--nojs');
+
+
+  function onOpenCloseMenu (list) {
+    if (list.classList.contains('page-footer__js-menu--closed')) {
+      list.classList.remove('page-footer__js-menu--closed');
+      list.classList.add('page-footer__js-menu--opened');
+    } else {
+      list.classList.add('page-footer__js-menu--closed');
+      list.classList.remove('page-footer__js-menu--opened');
+    }
+  };
+
+  var openCloseLists = function (toggle, list) {
+    toggle.addEventListener("click", function () {
+      onOpenCloseMenu (list);
+    });
+  };
+
+  for (var i = 0; i < footerToggles.length; i++) {
+    openCloseLists(footerToggles[i], lists[i]);
   }
-});
+
+})();
